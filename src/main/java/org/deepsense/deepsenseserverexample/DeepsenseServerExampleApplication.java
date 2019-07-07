0119be9caddfaaed8315861f02b19217a9e3890a
Bogdan Bocse <bogdan.bocse@gmail.com>
2019-07-07 15:07:01 +0300

    Initial commit. Boilerplate (skeleton) only.

---
+++ src/main/java/org/deepsense/deepsenseserverexample/DeepsenseServerExampleApplication.java
@@ -0,0 +1,13 @@
+package org.deepsense.deepsenseserverexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepsenseServerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepsenseServerExampleApplication.class, args);
+	}
+
+}
